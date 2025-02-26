9490ecdd79b3049edf494d0b1f78c1459e3e09ea
Luca Mazza <luca.mazza@mazluc.ch>
2025-02-26 21:37:19 +0100

    chore: base project

---
+++ src/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NMReporting;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NMReporting;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
